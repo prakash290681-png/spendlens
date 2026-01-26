94aa9023872a59e67d5742e22f0040f0b10c9276
Prakash <prakash290681@gmail.com>
2026-01-26 16:04:28 +0530

    Create DB tables on startup

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ print("CLIENT_ID:", os.getenv("GOOGLE_CLIENT_ID"))
 print("REDIRECT:", os.getenv("GOOGLE_REDIRECT_URI"))
 from fastapi import FastAPI
 from auth import router as auth_router
+from database import engine
+from models import Base
 
 app = FastAPI()
+Base.metadata.create_all(bind=engine)
 
 app.include_router(auth_router)
 

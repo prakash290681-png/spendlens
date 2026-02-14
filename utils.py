b2fff92ffc8f0adcd850ddfe71229a8f12a28ae3
Prakash <prakash290681@gmail.com>
2026-02-14 18:42:05 +0530

    separate rest and grocery

--- utils.py
+++ utils.py
@@ -1,13 +1,23 @@
 def detect_merchant(text):
+    print("🔥 DETECT MERCHANT CALLED 🔥")
+
     if not text:
         return None
 
+    print("TEXT SAMPLE:", text[:120])   # 👈 ADD THIS
+    
     t = text.lower()
 
     # 🥇 MOST SPECIFIC FIRST
     if "instamart" in t:
         return "Swiggy Instamart"
 
+    if "blinkit" in t:
+        return "Blinkit"
+
+    if "zepto" in t:
+        return "Zepto"
+    
     # 🥈 then general brands
     if "zomato" in t:
         return "Zomato"
@@ -18,7 +28,6 @@ def detect_merchant(text):
     return None
 
 
-
 def detect_category(merchant):
     if not merchant:
         return "Others"
@@ -26,7 +35,7 @@ def detect_category(merchant):
     m = merchant.lower()
 
     # groceries
-    if "instamart" in m:
+    if m in ["swiggy instamart", "blinkit", "zepto"]:
         return "Grocery"
 
     # restaurants
@@ -34,4 +43,5 @@ def detect_category(merchant):
         return "Restaurant"
 
     return "Others"
+
 
